ec114b9927c085eae459d9dd4aed7edb2ac57bec
AlexBinkovskyy <alex_bin@ukr.net>
2023-09-30 11:34:11 +0300

    edit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// У змінній min лежить число від 0 до 59. Визначте у котру чверть години
+// Попадає це число (у першу, другу, третю або четверту).
+
+// const min = parseInt(prompt("введіть число"));
+// if (min <= 15) {
+//   console.log("Перша чверть");
+// } else if (min <= 30) {
+//   console.log("Друга чверть");
+// } else if (min <= 45) {
+//   console.log("Третя чверть");
+// } else if (min <= 60) {
+//   console.log("Четверта чверть");
+// } else {
+//     console.log("Невірне значення");
+// }
+
+// const min = parseInt(prompt("введіть число"));
+// switch (true){
+//     case (min <= 15):
+//         console.log("Перша чверть");
+//         break;
+//     case (min <= 30):
+//         console.log("2 чверть");
+//         break;
+//     case (min <= 45):
+//         console.log("3 чверть");
+//         break;
+//     case (min <= 60):
+//         console.log("4 чверть");
+//         break;
+// }

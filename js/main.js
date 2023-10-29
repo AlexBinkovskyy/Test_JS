a29f680fbb8636c7d08c0a649001619a79735f7b
Misha <130393993+Misha2299@users.noreply.github.com>
2023-10-29 12:00:06 +0200

    задвчі

--- js/main.js
+++ js/main.js
@@ -290,17 +290,44 @@
   //з ​​таким ім'ям, ціною та кількістю з об'єкта
 
 
-  const stones = [
-    { name: "Смарагд", price: 1300, quantity: 4 },
-    { name: "Діамант", price: 2700, quantity: 6 },
-    { name: "Сапфір", price: 400, quantity: 7 },
-    { name: "Щебінь", price: 150, quantity: 100 },
-  ];
+//   const stones = [
+//     { name: "Смарагд", price: 1300, quantity: 4 },
+//     { name: "Діамант", price: 2700, quantity: 6 },
+//     { name: "Сапфір", price: 400, quantity: 7 },
+//     { name: "Щебінь", price: 150, quantity: 100 },
+//   ];
 
 
-function calcTotalPrice(stones, stonesName){
-  const{price, quantity} = stones.find((stone) => stone.name === stonesName)
-  return price * quantity
-}
+// function calcTotalPrice(stones, stonesName){
+//   const{price, quantity} = stones.find((stone) => stone.name === stonesName)
+//   return price * quantity
+// }
 
-console.log(calcTotalPrice(stones, "Діамант"));
+// console.log(calcTotalPrice(stones, "Діамант"));
+
+// Завдання 29.10.23
+
+
+//! 4. Виправте помилки, щоб код працював
+// const product = {
+//   price: 5000,
+//   showPrice() {
+//     console.log(this.price);
+//   },
+// };
+// product.showPrice();
+
+//! 5. Виправте помилки, щоб код працював
+
+// const item = {
+//     getQuantity() {
+//         console.log(this.quantity);
+//     },
+//     quantity: 5,
+// }
+
+// function callAction(action) {
+//       action.apply(item)
+//   }
+  
+// callAction(item.getQuantity);
